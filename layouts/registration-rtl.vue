8edd984d3943c234e1da620a471512221492a951
Koliaha <haquanghuy@mail.ru>
2023-04-04 01:05:14 +0300

    add loaded

--- layouts/registration-rtl.vue
+++ layouts/registration-rtl.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="registration-wrapper registration-wrapper-reverse">
+  <main class="registration-wrapper registration-wrapper-reverse"  v-cloak>
     <nuxt-link to="/" class="back reverse registration-back">
       <img src="@/assets/images/icons/arrow.svg" alt="arrow" />
     </nuxt-link>
@@ -7,3 +7,17 @@
     <lottie-registration class="registration-image" />
   </main>
 </template>
+<script>
+export default {
+  mounted() {
+    this.$nextTick(() => {
+      document.querySelector('[v-cloak]').removeAttribute('v-cloak');
+    });
+  },
+};
+</script>
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+</style>
--- layouts/registration.vue
+++ layouts/registration.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="registration-wrapper">
+  <main class="registration-wrapper" v-cloak>
     <nuxt-link to="/" class="back registration-back">
       <img src="@/assets/images/icons/arrow.svg" alt="arrow" />
     </nuxt-link>
@@ -7,3 +7,17 @@
     <forms-registration-credentials-form :is-rtl="false" />
   </main>
 </template>
+<script>
+export default {
+  mounted() {
+    this.$nextTick(() => {
+      document.querySelector('[v-cloak]').removeAttribute('v-cloak');
+    });
+  },
+};
+</script>
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+</style>
